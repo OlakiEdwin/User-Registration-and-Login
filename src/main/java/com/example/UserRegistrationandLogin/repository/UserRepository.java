82b14b8bc1a685afe68f2e3fbe4009a5d6164943
Olaki Edwin <olakiedwin@gmail.com>
2023-07-17 13:38:38 +0300

    Role repository

---
+++ src/main/java/com/example/UserRegistrationandLogin/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.UserRegistrationandLogin.repository;
+
+import net.javaguides.springboot.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+}
