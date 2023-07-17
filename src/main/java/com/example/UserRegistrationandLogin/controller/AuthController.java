bfdaa2f4d3db3f9fd429bd25aaeb7fa8090192c4
Olaki Edwin <olakiedwin@gmail.com>
2023-07-17 15:31:11 +0300

    Handler method to handle list of users

--- src/main/java/com/example/UserRegistrationandLogin/controller/AuthController.java
+++ src/main/java/com/example/UserRegistrationandLogin/controller/AuthController.java
@@ -1,8 +1,12 @@
 package com.example.UserRegistrationandLogin.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthController {
@@ -17,5 +21,32 @@ public class AuthController {
         UserDto user = new UserDto();
         model.add.addAttribute("user", user);
         return "register";
+    }
+
+    @PostMapping("/register/save")
+    public String registration(@Valid @ModelAttribute("user")UserDto,
+                               BindingResult result,
+                               Model model){
+        User extinguisher = userService.findUserByEmail(userDto.getEmail());
+
+        if(extinguisher != null && extinguisher.getEmail() != null && !extinguisher.getEmail().isEmpty()){
+            result.rejectValue("email", null,
+                    "There is already an account registered with the same email");
+        }
+
+        if(result.hasErrors()){
+            model.addAttribute("user", userDto);
+            return "/register";
+        }
+
+        userService.saveUser(userDto);
+        return "redirect:/register?sucess";
+    }
+
+    @GetMapping("/users")
+    public String users(Model model){
+        List<UserDto> users = userService.findAllUsers();
+        model.addAttribute("users", users);
+        return "users";
     }
 }
